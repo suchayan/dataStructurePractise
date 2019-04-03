d563303ba0854bd28fdbb2ff91e25198734c05c4
sucbiswas <sucbiswas@paypal.com>
2019-04-03 11:03:08 +0530

    Node created and linked

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,6 +1,6 @@
 class Node{
     int data;
-    Node head;
+    Node next;
     Node(int data){
         this.data=data;
     }
@@ -10,9 +10,13 @@ public class LinkedList {
 
     public static void main(String[] args) {
         Node head=new Node(1);
-        Node nodeA=new Node(1);
         Node nodeB=new Node(1);
         Node nodeC=new Node(1);
         Node nodeD=new Node(1);
+        //link setup
+        head.next=nodeB;
+        nodeB.next=nodeC;
+        nodeC.next=nodeD;
+
     }
 }
