55cc3f0e04fee06545252cfb225f02dbe17581d8
sucbiswas <sucbiswas@paypal.com>
2019-04-03 13:25:46 +0530

    adding generic insertNode functionality

--- src/SingleLinkedList.java
+++ src/SingleLinkedList.java
@@ -1,7 +1,7 @@
 
 public class SingleLinkedList {
 
-
+    static Node head;
     static class Node{
         int data;
         Node next;
@@ -11,7 +11,12 @@ public class SingleLinkedList {
         }
     }
 
+    void insertNode(int data){
+        Node new_node=new Node(data);
+        new_node.next=head;
+        head=new_node;
 
+    }
     void deleteNode(Node head){
 
     }
@@ -28,7 +33,7 @@ public class SingleLinkedList {
     }
 
     public static void main(String[] args) {
-        Node head=new Node(11);
+        /*Node head=new Node(11);
         Node nodeB=new Node(211);
         Node nodeC=new Node(321);
         Node nodeD=new Node(434);
@@ -36,12 +41,16 @@ public class SingleLinkedList {
         head.next=nodeB;
         nodeB.next=nodeC;
         nodeC.next=nodeD;
-
+*/
 //        System.out.println(countNodes(head));
 
 
    SingleLinkedList l1=new SingleLinkedList();
 
+//        System.out.println(l1.countNodes(head));
+
+        l1.insertNode(34);
+        l1.insertNode(36);
         System.out.println(l1.countNodes(head));
 
     }
