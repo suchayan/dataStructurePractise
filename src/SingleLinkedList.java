78064ff6dadfed015e04cb086bc5f22e80d20e0a
sucbiswas <sucbiswas@paypal.com>
2019-04-03 11:53:16 +0530

    Class name change to remove conflict

--- src/SingleLinkedList.java
+++ src/SingleLinkedList.java
@@ -1,17 +1,22 @@
+import java.util.LinkedList;
 class Node{
     int data;
     Node next;
     Node(int data){
         this.data=data;
+        next=null;
     }
 }
 
-public class LinkedList {
+public class SingleLinkedList {
+    void deleteNode(Node head){
+
+    }
 
     static int countNodes(Node head){
-        int count=1;
+        int count=0;
         Node current=head;
-        while(current.next!=null) {
+        while(current!=null) {
             System.out.println(current.data);
             current=current.next;
             count++;
@@ -20,16 +25,22 @@ public class LinkedList {
     }
 
     public static void main(String[] args) {
-        Node head=new Node(1);
-        Node nodeB=new Node(2);
-        Node nodeC=new Node(3);
-        Node nodeD=new Node(4);
+        Node head=new Node(11);
+        Node nodeB=new Node(211);
+        Node nodeC=new Node(321);
+        Node nodeD=new Node(434);
         //link setup
         head.next=nodeB;
         nodeB.next=nodeC;
         nodeC.next=nodeD;
 
         System.out.println(countNodes(head));
+
+
+   LinkedList l1=new LinkedList();
+        l1.push(1);
+
+
 
     }
 }
