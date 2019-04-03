fe311ea9178e39122b6f41ec09cd0465931a073c
sucbiswas <sucbiswas@paypal.com>
2019-04-03 10:51:08 +0530

    First Linked commit

---
+++ src/LinkedList.java
@@ -0,0 +1,18 @@
+class Node{
+    int data;
+    Node head;
+    Node(int data){
+        this.data=data;
+    }
+}
+
+public class LinkedList {
+
+    public static void main(String[] args) {
+        Node head=new Node(1);
+        Node nodeA=new Node(1);
+        Node nodeB=new Node(1);
+        Node nodeC=new Node(1);
+        Node nodeD=new Node(1);
+    }
+}
