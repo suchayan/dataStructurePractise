4c75a7a7dc2a1429139acf7a70dc7f4a9b172bad
sucbiswas <sucbiswas@paypal.com>
2019-04-03 11:18:42 +0530

    Traversing nodes

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -8,15 +8,28 @@ class Node{
 
 public class LinkedList {
 
+    static int countNodes(Node head){
+        int count=1;
+        Node current=head;
+        while(current.next!=null) {
+            System.out.println(current.data);
+            current=current.next;
+            count++;
+        }
+        return count;
+    }
+
     public static void main(String[] args) {
         Node head=new Node(1);
-        Node nodeB=new Node(1);
-        Node nodeC=new Node(1);
-        Node nodeD=new Node(1);
+        Node nodeB=new Node(2);
+        Node nodeC=new Node(3);
+        Node nodeD=new Node(4);
         //link setup
         head.next=nodeB;
         nodeB.next=nodeC;
         nodeC.next=nodeD;
+
+        System.out.println(countNodes(head));
 
     }
 }
